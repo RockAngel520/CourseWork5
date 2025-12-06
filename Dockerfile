fa367323573696f79dd20071a7faaba7da434efc
Anton-Z <rockangel520@mail.ru>
2025-12-06 21:59:55 +0300

    deploy

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,11 @@ FROM python:3.13-slim
 
 WORKDIR /app
 
+RUN apt-get update \\
+  && apt-get install -y gcc libpq-dev \\
+  && apt-get clean \\
+  && rm -rf /var/lib/apt/lists/\*
+
 RUN pip install poetry
 
 COPY pyproject.toml poetry.lock* ./
--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = True if os.getenv("DEBUG") == "True" else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
@@ -98,6 +98,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
